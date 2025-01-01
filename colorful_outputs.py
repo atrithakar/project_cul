c98fc25c732645f13352f24fd25eb80bd98cdc63
Atri Thakar <atrithaker122@gmail.com>
2025-01-01 23:24:26 +0530

    Added comments

--- colorful_outputs.py
+++ colorful_outputs.py
@@ -4,10 +4,46 @@ from colorama import Fore, init
 init(autoreset=True)
 
 def print_in_red(message):
+    '''
+    Prints the message in red color. Used for error messages.
+
+    Args:
+    None
+
+    Returns:
+    None
+
+    Raises:
+    None
+    '''
     print(Fore.RED + message)
 
 def print_in_yellow(text):
+    '''
+    Prints the message in yellow color. Used for warning messages.
+
+    Args:
+    None
+
+    Returns:
+    None
+
+    Raises:
+    None
+    '''
     print(Fore.YELLOW + text)
 
 def print_in_green(text):
+    '''
+    Prints the message in green color. Used for success messages.
+
+    Args:
+    None
+
+    Returns:
+    None
+
+    Raises:
+    None
+    '''
     print(Fore.GREEN + text)
