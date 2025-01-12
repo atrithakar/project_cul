10988c560051d78bbe9e212ee2b4d5fdb1e2360a
Atri Thakar <atrithaker122@gmail.com>
2025-01-12 11:44:21 +0530

    Added new helper function

--- helper_functions.py
+++ helper_functions.py
@@ -4,6 +4,10 @@ The purpose of this file is to make it easier to manage the functions that are u
 These functions are the ones which are tiny and are used in multiple files.
 '''
 
+from colorful_outputs import print_in_red
+import os
+import chardet
+
 def ensure_dir_exists(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
@@ -13,3 +17,18 @@ def parse_module(module_name_str):
 
 def check_if_dir_exists(directory):
     return os.path.exists(directory)
+
+def handle_req_file_ops(file_path, func):
+    try:
+        with open(file_path,'rb') as f:
+            raw_data = f.read()
+            result = chardet.detect(raw_data)
+            content = raw_data.decode(result['encoding'])
+            for line in content.split('\n'):
+                if(line == ''):
+                    continue
+                func(line.strip())
+    except FileNotFoundError:
+        print_in_red(f"Error: {file_path} not found.")
+    except Exception as e:
+        print_in_red(f"Error: {e}")
