cfbbc253cedc60dc43ac7b404a7859dd4585d187
Atri Thakar <atrithaker122@gmail.com>
2025-02-16 15:00:56 +0530

    Removed unnecessary functions

--- helper_functions.py
+++ helper_functions.py
@@ -3,20 +3,12 @@ This file contains the helper functions used in the project.
 The purpose of this file is to make it easier to manage the functions that are used in multiple files.
 These functions are the ones which are tiny and are used in multiple files.
 '''
-
 from colorful_outputs import print_in_red
 import os
 import chardet
 
-def ensure_dir_exists(directory: str):
-    if not os.path.exists(directory):
-        os.makedirs(directory, exist_ok=True)
-
 def parse_module(module_name_str: str):
     return module_name_str.split("==") if "==" in module_name_str else [module_name_str, ""]
-
-def check_if_dir_exists(directory):
-    return os.path.exists(directory)
 
 def handle_req_file_ops(file_path: str, func: callable):
     try:
