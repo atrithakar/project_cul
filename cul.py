20133ecee3cb35574655406c91396d7aebb3a68a
Atri Thakar <atrithaker122@gmail.com>
2025-02-17 18:11:35 +0530

    Modularized the code - Created a separate module for help_message function

--- cul.py
+++ cul.py
@@ -7,48 +7,9 @@ from freeze_requirements import freeze, list_modules
 from search_module import search_module
 from init import init
 from helper_functions import handle_req_file_ops
+from cul_help import help_message
 import codecs
 import chardet
-# imports all the required functions from the various files and modules
-
-
-def help_message():
-    '''
-    Prints the help message for the cul CLI.
-
-    Args:
-        None
-    
-    Returns:
-        None
-    
-    Raises:
-        None
-    '''
-    help_text = """
-    cul CLI - Command Line Interface for managing C/C++ libraries
-    
-    Commands:
-        install library                            - Installs the specified C/C++ library.
-        install library==version                   - Installs the specified version of the library.
-        install library1 library2 library3 ...     - Installs multiple libraries. (Version is optional and can be specified for each library)
-        uninstall library                          - Uninstalls the specified library.
-        uninstall library1, library2, library3 ... - Uninstalls multiple libraries.
-        update library                             - Updates the specified library to the latest version.
-        update library1, library2, library3 ...    - Updates multiple libraries to the latest version.
-        search library                             - Searches for the specified library and displays available versions.
-        list                                       - Lists all installed libraries.
-        freeze                                     - Outputs the installed libraries in requirements.txt format.
-        help                                       - Shows the help message.
-        cache clear                                - Clears the cache of the installed libraries.
-        cache show                                 - Shows the cached modules along with their versions.
-        init                                       - Initializes the project with a module_info.json file.
-        init -y                                    - Initializes the project with a module_info.json file without asking for confirmation.
-
-    MADE WITH ❤️ BY ATRI THAKAR
-    """
-    print(help_text)
-
 
 def main():
     # Check if the user has provided a command
