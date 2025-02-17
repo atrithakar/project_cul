b179fadf413271da36025961fac5efec9c457419
Atri Thakar <atrithaker122@gmail.com>
2025-02-17 18:19:13 +0530

    Refined the help message

--- cul_help.py
+++ cul_help.py
@@ -12,24 +12,27 @@ def help_message():
         None
     '''
     help_text = """
-    cul CLI - Command Line Interface for managing C/C++ libraries
+    cul CLI - Command Line Interface for managing C/C++ modules
     
     Commands:
-        install library                            - Installs the specified C/C++ library.
-        install library==version                   - Installs the specified version of the library.
-        install library1 library2 library3 ...     - Installs multiple libraries. (Version is optional and can be specified for each library)
-        uninstall library                          - Uninstalls the specified library.
-        uninstall library1, library2, library3 ... - Uninstalls multiple libraries.
-        update library                             - Updates the specified library to the latest version.
-        update library1, library2, library3 ...    - Updates multiple libraries to the latest version.
-        search library                             - Searches for the specified library and displays available versions.
-        list                                       - Lists all installed libraries.
-        freeze                                     - Outputs the installed libraries in requirements.txt format.
+        install module                             - Installs the specified C/C++ module.
+        install module==version                    - Installs the specified version of the module.
+        install module1 module2 module3 ...        - Installs multiple modules. (Version is optional and can be specified for each module)
+        uninstall module                           - Uninstalls the specified module.
+        uninstall module1, module2, module3 ...    - Uninstalls multiple modules.
+        update module                              - Updates the specified module to the latest version (available in either server or cache, depending upon the situation).
+        update module1, module2, module3 ...       - Updates multiple modules to the latest version.
+        search module                              - Searches for the specified module and displays available versions.
+        list                                       - Lists all installed modules in human readable format.
+        freeze                                     - Lists all installed modules in machine readable format.
         help                                       - Shows the help message.
-        cache clear                                - Clears the cache of the installed libraries.
+        cache clear                                - Removes the stored cache data for the installed modules.
         cache show                                 - Shows the cached modules along with their versions.
         init                                       - Initializes the project with a module_info.json file.
         init -y                                    - Initializes the project with a module_info.json file without asking for confirmation.
+
+    NOTE: To store the output of any command into a file, use the following syntax:
+        cul <command> > <filename>.txt
 
     MADE WITH ❤️ BY ATRI THAKAR
     """
