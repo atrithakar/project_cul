5b2db3920d8a32eefdddbb2ed0ab008a8b4378bf
Atri Thakar <atrithaker122@gmail.com>
2025-02-18 17:21:53 +0530

    Added a new variable

--- common_variables.py
+++ common_variables.py
@@ -8,3 +8,4 @@ BASE_URL = "https://culb.vercel.app"
 # BASE_URL = "http://192.168.0.104:5000"
 C_CPP_MODULES_DLD_DIR = "./c_cpp_modules_dld"
 CACHE_DIR = appdirs.user_cache_dir("CUL", "CUL_CLI")
+CUL_DIR = ".cul"
