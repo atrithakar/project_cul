6daa62749c7b174557809a8d9bcb4d678878a6a0
Atri Thakar <atrithaker122@gmail.com>
2025-02-19 11:06:47 +0530

    Updated the help message

--- cul_help.py
+++ cul_help.py
@@ -18,13 +18,19 @@ def help_message():
         install module                             - Installs the specified C/C++ module.
         install module==version                    - Installs the specified version of the module.
         install module1 module2 module3 ...        - Installs multiple modules. (Version is optional and can be specified for each module)
+        install -r requirements.txt                 - Installs the modules specified in the requirements.txt file.
         uninstall module                           - Uninstalls the specified module.
         uninstall module1, module2, module3 ...    - Uninstalls multiple modules.
+        uninstall -r requirements.txt               - Uninstalls the modules specified in the requirements.txt file, will ignore the versions specified in the file.
         update module                              - Updates the specified module to the latest version (available in either server or cache, depending upon the situation).
         update module1, module2, module3 ...       - Updates multiple modules to the latest version.
+        update -r requirements.txt                  - Updates the modules specified in the requirements.txt file, will ignore the versions specified in the file.
         search module                              - Searches for the specified module and displays available versions.
+        search --fuzzy module                      - Searches for the specified module using fuzzy search and displays the modules with similar names.
         list                                       - Lists all installed modules in human readable format.
+        list > requirements.txt                    - Lists all installed modules in human readable format and stores the output in a file.
         freeze                                     - Lists all installed modules in machine readable format.
+        freeze > requirements.txt                  - Lists all installed modules in machine readable format and stores the output in a file.
         help                                       - Shows the help message.
         cache clear                                - Removes the stored cache data for the installed modules.
         cache show                                 - Shows the cached modules along with their versions.
