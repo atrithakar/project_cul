8f3596206af6b19e91a36b8fe77e495c5f0447c7
Atri Thakar <atrithaker122@gmail.com>
2025-02-20 23:46:10 +0530

    Updated the help message

--- cul_help.py
+++ cul_help.py
@@ -19,8 +19,8 @@ def help_message():
         install module==version                                       - Installs the specified version of the module.
         install module1, module2, module3 ...                         - Installs multiple modules. (Version is optional and can be specified for each module)
         install -r requirements.txt                                   - Installs the modules specified in the requirements.txt file.
-        install module1, module2, module3 --use-reg <registry_url>    - Installs the specified module from the specified registry.
-        install -r requirements.txt --use-reg <registry_url>          - Installs the modules specified in the requirements.txt file from the specified registry.
+        install module1, module2, module3 --use-reg "registry_url"    - Installs the specified module from the specified registry.
+        install -r requirements.txt --use-reg "registry_url"          - Installs the modules specified in the requirements.txt file from the specified registry.
 
         uninstall module                                              - Uninstalls the specified module.
         uninstall module1, module2, module3 ...                       - Uninstalls multiple modules.
@@ -28,14 +28,14 @@ def help_message():
 
         update module                                                 - Updates the specified module to the latest version (available in either server or cache, depending upon the situation).
         update module1, module2, module3 ...                          - Updates multiple modules to the latest version.
-        update module1, module2, module3 ... --use-reg <registry_url> - Updates multiple modules from the specified registry.
+        update module1, module2, module3 ... --use-reg "registry_url" - Updates multiple modules from the specified registry.
         update -r requirements.txt                                    - Updates the modules specified in the requirements.txt file, will ignore the versions specified in the file.
-        update -r requirements.txt --use-reg <registry_url>           - Updates the modules specified in the requirements.txt file from the specified registry.
+        update -r requirements.txt --use-reg "registry_url"           - Updates the modules specified in the requirements.txt file from the specified registry.
         
         search module                                                 - Searches for the specified module and displays available versions.
-        search module --use-reg <registry_url>                        - Searches for the specified module from the specified registry and displays available versions.
+        search module --use-reg "registry_url"                        - Searches for the specified module from the specified registry and displays available versions.
         search --fuzzy module                                         - Searches for the specified module using fuzzy search and displays the modules with similar names.
-        search --fuzzy module --use-reg <registry_url>                - Searches for the specified module using fuzzy search from the specified registry and displays the modules with similar names.
+        search --fuzzy module --use-reg "registry_url"                - Searches for the specified module using fuzzy search from the specified registry and displays the modules with similar names.
         
         list                                                          - Lists all installed modules in human readable format.
         list > requirements.txt                                       - Lists all installed modules in human readable format and stores the output in a file.
@@ -52,7 +52,8 @@ def help_message():
         init -y                                                       - Initializes the project with a module_info.json file without asking for confirmation.
 
     NOTE: To store the output of any command into a file, use the following syntax:
-        cul <command> > <filename>.txt
+            cul <command> > <filename>.txt
+          The registry URL must be enclosed in double quotes or single quotes and must contain the protocol (http/https).
 
     MADE WITH ❤️ BY ATRI THAKAR
     """
