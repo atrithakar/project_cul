96ed2152eadb56c56e4bca7124c5ac449bdb3261
Atri Thakar <atrithaker122@gmail.com>
2025-08-22 22:17:39 +0530

    Made some changes for the ease of debugging

--- common_variables.py
+++ common_variables.py
@@ -7,6 +7,10 @@ import appdirs
 DEFAULT_URL = "https://cul-backend-fastapi-mongodb.onrender.com" # Default URL for the CUL backend, change to your local server address if needed
 BASE_URL = "https://cul-backend-fastapi-mongodb.onrender.com" # Default URL for the CUL backend, change to your local server address if needed
 
+# For debugging purposes, you can uncomment the following lines to use a local server
+# DEFAULT_URL = "http://192.168.0.100:8000" # Default URL for the CUL backend, change to your local server address if needed
+# BASE_URL = "http://192.168.0.100:8000" # Default URL for the CUL backend, change to your local server address if needed
+
 try:
     with open('.cul/cul_config.txt', 'r') as f:
         text = f.read().strip()
